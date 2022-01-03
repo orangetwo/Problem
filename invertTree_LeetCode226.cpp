d105da8b21470cf96e962bde8dc92f1d560c7000
lisi06 <lisi06@58.com>
2022-01-03 11:13:20 +0800

    翻转二叉树 LeetCode 226

---
+++ invertTree_LeetCode226.cpp
@@ -0,0 +1,31 @@
+//
+// Created by 李思 on 2022/1/3.
+//
+
+
+using namespace std;
+
+struct TreeNode {
+        int val;
+        TreeNode *left;
+        TreeNode *right;
+        TreeNode() : val(0), left(nullptr), right(nullptr) {}
+        TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+        TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    };
+
+class Solution{
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (root == nullptr) return nullptr;
+
+        TreeNode *left = invertTree(root->left);
+        TreeNode *right = invertTree(root->right);
+
+        root->left = right;
+        root->right = left;
+
+        return root;
+
+    }
+};
--- test.cpp
+++ test.cpp
@@ -3,3 +3,26 @@
 //
 
 
+#include "iostream"
+using namespace std;
+
+void modifyValue(int x){
+    // 这里我们想修改x的值
+    x = 0;
+}
+
+void modifyValue(int *x){
+    *x = 0;
+}
+
+int main(){
+
+    int x = 1;
+
+    modifyValue(x);
+    cout << x << endl;
+    modifyValue(&x);
+    cout << x << endl;
+
+    return 1;
+}
