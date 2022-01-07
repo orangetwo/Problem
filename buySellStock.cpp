47f11bd09a19a315c88ead43a10dca881be4d359
orange <leesifox@gmail.com>
2022-01-07 09:43:41 +0800

    允许交易一次时股票买卖 LeetCode 121

--- buySellStock.cpp
+++ buySellStock.cpp
@@ -35,3 +35,27 @@
  *              dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i])
  *              dp[i][1] = max(dp[i-1][1], - prices[i])
  */
+
+#include "vector"
+using namespace std;
+
+int maxProfit(vector<int> prices){
+
+    if(prices.empty()) return 0;
+
+    int profit = 0;
+    int minPrice = prices[0];
+    for(auto price : prices){
+        profit = max(profit, price - minPrice);
+
+        if (price < minPrice) minPrice = price;
+    }
+    return profit;
+}
+
+/*
+ * k = 2时,
+ *          dp[i][0][0] = 0
+ *          dp[i][0][1] = -∞
+ *          dp[]
+ */
